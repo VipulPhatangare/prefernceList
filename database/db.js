7ed4e48fb3568926a7fd5042bd019de14f7381be
VipulPhatanagare <vipulphatangare3@gmail.com>
2025-07-12 12:55:15 +0530

    adding video

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('VP MongoDB connected');
+  } catch (err) {
+    console.error('VP MongoDB connection failed:', err.message);
+    process.exit(1);
+  }
+};
